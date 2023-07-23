47f3e8fe9babd87d4c536f71f3dfdb8eeb401cca
Adam Haile <adammhaile@gmail.com>
2023-07-23 17:00:18 +0200

    Cleanup code

--- abmgr/__init__.py
+++ abmgr/__init__.py
@@ -52,51 +52,7 @@ class ABMgr():
         
         save_path = Path(self.__app_config.full_path).absolute().as_posix()
         print(f'\nBase configuration saved to: {save_path}')
-    
-    def __get_oa_books(self, oa_lib_dir):
-        books_path = oa_lib_dir / 'books'
-        
-        results = []
-        
-        #print('Scanning books...\n')
-        
-        for b in books_path.iterdir():
-            if b.suffix.lower() == '.m4b':
-                meta = TinyTag.get(str(b.resolve()))
-                short_title = str(meta.album)
-                author = str(meta.artist)
-                long_title = str(meta.title)
-                series = None
-                book_num = None
-                
-                if long_title != short_title:
-                    series = long_title[(len(short_title) + 3):]
-
-                    if series.endswith(')'):
-                        start = series.find('(')
-                        series = series[start+1:-1]
-                        
-                    if series.startswith('Book '):
-                        book_num = series[5:]
-                        series = short_title
-                    else:
-                        for split in SERIES_SPLIT:
-                            if split in series:
-                                series, book_num = series.split(split)
-                            
-                book = {
-                    'path': str(b.as_posix()),
-                    'author': author,
-                    'title': long_title,
-                    'short_title': short_title,
-                    'series': series,
-                    'book_num': book_num
-                }
-
-                results.append(book)
-
-        return results
-        
+       
     def sync(self):
         """Read books from OpenAudible"""
         paths = self.__app_config['paths']
@@ -104,7 +60,6 @@ class ABMgr():
         abs_lib_dir = Path(paths['abs_lib_dir'])
         books_path = oa_lib_dir / 'books'
         
-        # books = self.__get_oa_books(oa_lib_dir)
         books = self.__read_oa_library(oa_lib_dir)
 
         print('Copying to output...')
@@ -129,8 +84,6 @@ class ABMgr():
                 save_path = save_path / clean(series) / clean(book_title)
             else:
                 save_path = save_path / clean(title)
-                
-            print(f'{src_path} -->\n{save_path.as_posix()}/{filename}\n')
             
             if not save_path.exists():
                 # print(f'mkdir {save_path}')
@@ -138,6 +91,7 @@ class ABMgr():
                 
             m4b_path = save_path / filename
             if(not m4b_path.exists()):
+                print(f'{src_path} -->\n{save_path.as_posix()}/{filename}\n')
                 Path(src_path).link_to(m4b_path)
                 #m4b_path.hardlink_to(src_path) #python 3.10 version
                 
@@ -169,16 +123,6 @@ class ABMgr():
             results.append(book)
             
         return results
-    
-    def read(self):
-        paths = self.__app_config['paths']
-        oa_lib_dir = Path(paths['oa_lib_dir'])
-        abs_lib_dir = Path(paths['abs_lib_dir'])
-        books_path = oa_lib_dir / 'books'
-        
-        books = self.__read_oa_library(oa_lib_dir)
-        
-        print(json.dumps(books, indent=2))
         
 def main():
     fire.Fire(ABMgr)
